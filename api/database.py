23b78c58f0535c10887b2c23b906425623d21eb3
Jakob Kranawetter <jkranawetter05@gmail.com>
2022-02-13 15:01:13 +0100

    Migrate to postgres

--- api/database.py
+++ api/database.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['DEBUG'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app=app, session_options={'autoflush': False})
-
+migrate = Migrate(app, db)
