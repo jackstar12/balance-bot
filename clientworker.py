4807d193b3b6605b6defcd9499d72bb5e030f652
jackstar12 <jkranawetter05@gmail.com>
2022-03-11 12:30:44 +0100

    Fix Balance Fetching

--- clientworker.py
+++ clientworker.py
@@ -29,10 +29,10 @@ class ClientWorker:
         self._identifier = id
         self._last_fetch = datetime.fromtimestamp(0)
 
-    def get_balance(self, time: datetime = None):
+    def get_balance(self, time: datetime = None, force=False):
         if not time:
             time = datetime.now()
-        if time - self._last_fetch < timedelta(seconds=30):
+        if time - self._last_fetch < timedelta(seconds=30) and not force:
             return None
         else:
             self._last_fetch = time
