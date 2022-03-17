068cb6fd4ed4efa605321594e86e11f854fb38e6
Jakob Kranawetter <jkranawetter05@gmail.com>
2022-03-17 17:39:40 +0100

    Safety

--- usermanager.py
+++ usermanager.py
@@ -133,11 +133,12 @@ class UserManager(Singleton):
 
     def _get_worker(self, client: Client, create_if_missing=True) -> ClientWorker:
         with self._worker_lock:
-            worker = self._workers_by_client_id.get(client.id)
-            if not worker and create_if_missing:
-                self.add_client(client)
+            if client:
                 worker = self._workers_by_client_id.get(client.id)
-            return worker
+                if not worker and create_if_missing:
+                    self.add_client(client)
+                    worker = self._workers_by_client_id.get(client.id)
+                return worker
 
     def _get_worker_event(self, user_id, guild_id):
         with self._worker_lock:
@@ -214,7 +215,6 @@ class UserManager(Singleton):
                         balance = Balance(amount=0.0, currency='$', extra_currencies={}, error=None, time=time)
                     else:
                         balance = worker.get_balance(time, force=force_fetch)
-
                     history_len = len(client.history)
                     latest_balance = None if history_len == 0 else client.history[history_len - 1]
                     if balance:
