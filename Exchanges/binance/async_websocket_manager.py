8d37528368a04b9dc06ca587ae899637ce6951d4
jackstar12 <jkranawetter05@gmail.com>
2022-03-24 15:41:38 +0100

    Update websockets (school sucks kekw)

---
+++ Exchanges/binance/async_websocket_manager.py
@@ -0,0 +1,90 @@
+import asyncio
+import json
+import logging
+import time
+from threading import Thread, Lock
+import aiohttp
+import threading
+
+import websockets
+from websocket import WebSocketApp
+
+
+class WebsocketManager:
+    _CONNECT_TIMEOUT_S = 5
+
+    def __init__(self, session: aiohttp.ClientSession):
+        self.connect_lock = Lock()
+        self._ws = None
+        self._session = session
+
+    def send(self, message):
+        self.connect()
+        self._ws.send(message)
+
+    async def send_json(self, data):
+        if self._ws and not self._ws.closed:
+            return await self._ws.send_json(data)
+
+    def reconnect(self) -> None:
+        if self.connected:
+            self._reconnect(self._ws)
+
+    def connect(self):
+        if self.connected:
+            return
+        asyncio.create_task(self._async_connect())
+
+    @property
+    def connected(self):
+        return self._ws and not self._ws.closed
+
+    async def _async_connect(self):
+        async with self._session.ws_connect(self._get_url(), autoping=True) as ws:
+            self._ws = ws
+            async for msg in ws:
+                print('Message received from server:', msg)
+                if msg.type == aiohttp.WSMsgType.PING:
+                    await ws.pong()
+                elif msg.type == aiohttp.WSMsgType.TEXT:
+                    self._callback(self._on_message, ws, msg.data)
+                elif msg.type == aiohttp.WSMsgType.ERROR:
+                    self._callback(self._on_close, ws)
+                    break
+                elif msg.type == aiohttp.WSMsgType.CLOSED:
+                    self._callback(self._on_close, ws)
+                    break
+
+    def _callback(self, f, ws, *args, **kwargs):
+        if callable(f) and ws is self._ws:
+            try:
+                f(ws, *args, **kwargs)
+            except Exception:
+                logging.exception('Error running websocket callback:')
+
+    def _run_websocket(self, ws):
+        try:
+            ws.run_forever()
+        except Exception as e:
+            raise Exception(f'Unexpected error while running websocket: {e}')
+        finally:
+            self._reconnect(ws)
+
+    async def _reconnect(self, ws):
+        assert ws is not None, '_reconnect should only be called with an existing ws'
+        if ws is self._ws:
+            self._ws = None
+            await ws.close()
+            self.connect()
+
+    def _on_close(self, ws):
+        self._reconnect(ws)
+
+    def _on_error(self, ws, error):
+        self._reconnect(ws)
+
+    def _get_url(self):
+        raise NotImplementedError()
+
+    def _on_message(self, ws, message):
+        raise NotImplementedError()
--- Exchanges/binance/futures_websocket_client.py
+++ Exchanges/binance/futures_websocket_client.py
@@ -7,20 +7,19 @@ from datetime import timedelta
 from threading import Timer, Lock
 from typing import Callable
 
-from Exchanges.binance.websocket_manager import WebsocketManager
+from Exchanges.binance.async_websocket_manager import WebsocketManager
 from datetime import datetime
 import aiohttp
 
+
 # https://binance-docs.github.io/apidocs/futures/en/#user-data-streams
 class FuturesWebsocketClient(WebsocketManager):
-
     _ENDPOINT = 'wss://fstream.binance.com'
 
     def __init__(self, client, session: aiohttp.ClientSession, on_message: Callable = None):
         super().__init__(session=session)
         self._client = client
         self._listenKey = None
-        self._key_lock = Lock()
         self._keep_alive_timer = None
         self._on_message = on_message
 
@@ -41,19 +40,15 @@ class FuturesWebsocketClient(WebsocketManager):
             await self.connect()
 
     def stop(self):
-        with self._key_lock:
-            self._listenKey = None
+        self._listenKey = None
 
-    def _renew_listen_key(self):
-        with self._key_lock:
-            self._listenKey = asyncio.run(self._client.start_user_stream())
-        self.reconnect()
+    async def _renew_listen_key(self):
+        self._listenKey = asyncio.run(self._client.start_user_stream())
 
     def _keep_alive(self):
-        with self._key_lock:
-            if self._listenKey:
-                logging.info('Trying to reconnect binance websocket')
-                self._listenKey = self._client.start_user_stream()
-                keep_alive = Timer(timedelta(minutes=45).total_seconds(), self._keep_alive)
-                keep_alive.daemon = True
-                keep_alive.start()
+        if self._listenKey:
+            logging.info('Trying to reconnect binance websocket')
+            self._listenKey = self._client.start_user_stream()
+            keep_alive = Timer(timedelta(minutes=45).total_seconds(), self._keep_alive)
+            keep_alive.daemon = True
+            keep_alive.start()
