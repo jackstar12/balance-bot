7fc102653340d78e9986bab94758e63ad11eeec9
jackstar12 <jkranawetter05@gmail.com>
2022-06-02 08:08:43 +0200

    binance impl

--- Exchanges/binance/binance.py
+++ Exchanges/binance/binance.py
@@ -71,8 +71,15 @@ class BinanceFutures(_BinanceBaseClient):
     async def _get_balance(self, time: datetime = None):
         response = await self._get('/fapi/v2/account')
 
+        usd_assets = [
+            asset for asset in response["assets"] if asset["asset"] in ("USDT", "BUSD")
+        ]
+
         return balance.Balance(
-            amount=float(response.get('availableBalance', 0)),
+            amount=sum(
+                float(asset.get('walletBalance', 0))
+                for asset in usd_assets
+            ),
             currency='$',
             time=time if time else datetime.now(),
             error=response.get('msg', None)
