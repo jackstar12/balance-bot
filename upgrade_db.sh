921b19fe7985c3262a8f595c07c1f0ed71ab627b
Jakob Kranawetter <jkranawetter05@gmail.com>
2022-06-26 18:20:06 +0200

    update upgrade script

--- upgrade_db.sh
+++ upgrade_db.sh
@@ -1,3 +1,3 @@
 docker build --tag balancebot_common --file balancebot/common/Dockerfile .
-docker-compose build && docker-compose up migrate
-docker-compose up
+docker-compose build migrate
+docker-compose up migrate
