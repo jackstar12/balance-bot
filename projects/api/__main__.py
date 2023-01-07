20bdc1a36882caaaaf034ed32857f9e40645a971
Jakob Kranawetter <jkranawetter05@gmail.com>
2023-01-07 15:17:43 +0100

    test uvicorn run

--- projects/api/__main__.py
+++ projects/api/__main__.py
@@ -1,11 +1,9 @@
 import uvicorn
-
-from api.app import app
 from os import environ
 
 
 def run():
-    uvicorn.run(app, host='localhost', port=environ.get('PORT', 5000), workers=4)
+    uvicorn.run('api.app', host='localhost', port=environ.get('PORT', 5000), workers=4)
 
 
 if __name__ == '__main__':
